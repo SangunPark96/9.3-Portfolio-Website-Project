d87ef0b5973cca68ca97001426f389dab057a372
SangUn Park <Sangunpark1@gmail.com>
2023-01-16 18:54:42 -0500

    messing with hover lol

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # 9.3-Pursuit-Portfolio-Project
 This is my Js, CSS, and HTML Codes for my public news aggregator website. It is targeting the public NewsAPI to gather latest news headlines based on the users result.
-Just go to https://clinquant-starlight-829cb6.netlify.app/ and on load, the top article for USA will automically populate on the left side of the page, then the right side has a form you can search any topic and it will populate the top 3 articles relative to the search.
+
+
+While it is currently deployed on netlify with the domain https://clinquant-starlight-829cb6.netlify.app/, recent documentation has brought to light a change in NewsAPI's "free" developer liberties. To circumvent this I must either pay or do a number of things with flask on the back end but as that is outside of the scope of the cohort, I will just demonstrate the functionality locally. 
+
+
+To use just load the page and immediately it will generate the top story at the moment on the left hand side of the page. On the right is a search form that will allow users to enter any particular topic and the page will generate the top 3 stories, as well as another image that allows the user to jump to the source article.
--- main.js
+++ main.js
@@ -60,11 +60,16 @@ const h2 = document.createElement("h2");
 const p1 = document.createElement("p");
 const a1 = document.createElement("a")
 const img1 = document.createElement("img")
+const p8 = document.createElement("p")
+const p9 = document.createElement("p")
+const p10 = document.createElement("p")
+const p11 = document.createElement("p")
 
 //will focus on search functionality 
 function getTopStories(newsData) {
     const topTitle = newsData.articles[0].title
     h2.innerHTML = topTitle
+    p8.innerHTML = `<strong> By ${newsData.articles[0].source.name} </strong>`
     const topDescription = newsData.articles[0].description
     p1.textContent = topDescription
     img1.setAttribute("src", newsData.articles[0].urlToImage)
@@ -73,7 +78,7 @@ function getTopStories(newsData) {
     img1.setAttribute("alt", newsData.articles[0].url)
     a1.setAttribute("href", newsData.articles[0].url)
     a1.append(img1)
-    mainSection.append(h2, p1, a1)
+    mainSection.append(h2, p8, p1, a1)
 }
 
 
@@ -113,38 +118,41 @@ const img4 = document.createElement("img")
 
 function getNewArticles(searchData){
     p2.innerHTML = `<h2>${searchData.articles[0].title}</h2>`
+    p9.innerHTML =`<strong> By ${searchData.articles[0].source.name} </strong>`
     p3.textContent = searchData.articles[0].description
     img2.setAttribute("src", searchData.articles[0].urlToImage)
     img2.setAttribute("width", "auto")
     img2.setAttribute("height", 175)
+    img2.setAttribute("alt", searchData.articles[0].url)
     a2.setAttribute("href", searchData.articles[0].url)
-    a2.innerHTML = "Link to Article"
     a2.append(img2)
 
-    div1.append(p2, p3, a2)
+    div1.append(p2, p9, p3, a2)
     newsSearchdiv.append(div1)
 
 
     p4.innerHTML = `<h2>${searchData.articles[1].title}</h2>`
+    p10.innerHTML =`<strong> By ${searchData.articles[1].source.name} </strong>`
     p5.textContent = searchData.articles[1].description
     img3.setAttribute("src", searchData.articles[1].urlToImage)
     img3.setAttribute("width", "auto")
     img3.setAttribute("height", 175)
+    img3.setAttribute("alt", searchData.articles[1].url)
     a3.setAttribute("href", searchData.articles[1].url)
-    a3.innerHTML = "Link to Article"
     a3.append(img3)
-    div2.append(p4, p5, a3)
+    div2.append(p4, p10, p5, a3)
     newsSearchdiv.append(div2)
 
     p6.innerHTML = `<h2>${searchData.articles[2].title}</h2>`
+    p11.innerHTML =`<strong> By ${searchData.articles[2].source.name} </strong>`
     p7.textContent = searchData.articles[2].description
     img4.setAttribute("src", searchData.articles[2].urlToImage)
     img4.setAttribute("width", "auto")
     img4.setAttribute("height", 175)
+    img4.setAttribute("alt", searchData.articles[2].url)
     a4.setAttribute("href", searchData.articles[2].url)
-    a4.innerHTML = "Link to Article"
     a4.append(img4)
-    div3.append(p6, p7, a4)
+    div3.append(p6, p11, p7, a4)
     newsSearchdiv.append(div3)
 }
   
